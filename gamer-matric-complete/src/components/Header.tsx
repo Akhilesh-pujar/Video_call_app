d213dda19be349580bbfa3ce50446d204e9549c1
Akhilesh-pujar <akhileshpujar796@gmail.com>
2024-12-05 16:48:36 +0530

    zego_update

--- gamer-matric-complete/src/components/Header.tsx
+++ gamer-matric-complete/src/components/Header.tsx
@@ -68,7 +68,7 @@ export default function Header() {
         <Link to="/">
           <EuiText>
             <h2 style={{ padding: "0 1vw" }}>
-              <EuiTextColor color="#0b5cff">gamer-matric-assignment</EuiTextColor>
+              <EuiTextColor color="#0b5cff">P2P comm & conff</EuiTextColor>
             </h2>
           </EuiText>
         </Link>,
@@ -137,7 +137,7 @@ export default function Header() {
         <Link to="/">
           <EuiText>
             <h2 style={{ padding: "0 1vw" }}>
-              <EuiTextColor color="#0b5cff">Zoom</EuiTextColor>
+              <EuiTextColor color="#0b5cff">P2P comm & conff</EuiTextColor>
             </h2>
           </EuiText>
         </Link>,
